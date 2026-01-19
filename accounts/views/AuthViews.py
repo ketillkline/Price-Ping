bd832068df8a7616af3d3b12420871ef0c4ee372
Joshua McGrath-Rodriguez <jrod4601@gmail.com>
2026-01-19 10:16:13 -0500

    Added authentication for the user

--- accounts/views/AuthViews.py
+++ accounts/views/AuthViews.py
@@ -1,12 +1,10 @@
 from django.http import HttpRequest
 from django.shortcuts import render, redirect
-from rest_framework.response import Response
-
-from django.contrib.auth import get_user_model
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login, logout, get_user_model
 
 
 def login_view(request: HttpRequest):
+    # take out set
     errors = set()
     template_name = "login.html"
     match request.method:
@@ -19,6 +17,13 @@ def login_view(request: HttpRequest):
             if not username or not password:
                 errors.add("Please fill in all required fields")
                 return render(request, template_name, {"errors": errors})
+            user = authenticate(request, username=username, password=password)
+            if not user:
+                errors.add("Account does not exist")
+                return render(request, template_name, {"errors": errors})
+            login(request, user)
+            return redirect("dashboard")
+
 
 
 def signup_view(request: HttpRequest):
