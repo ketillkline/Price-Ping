170cf304b67cf6930f73a3a7b2a600bd1901b4db
Joshua McGrath-Rodriguez <jrod4601@gmail.com>
2026-01-24 09:58:07 -0500

    Created a test user with an appropriate password

--- e2e/conftest.py
+++ e2e/conftest.py
@@ -3,3 +3,13 @@ import os
 from playwright.sync_api import sync_playwright
 from django.contrib.auth.models import User
 
+@pytest.fixture(scope="session")
+def test_user(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        return User.objects.create_user(
+            username="testuser", password="Testuser1*")
+
+@pytest.fixture(scope="session")
+def browser(test_user):
+    pass
+
