02a8d706690d91d9f2db72dc8e5c7e9f4c4740cd
Joshua McGrath-Rodriguez <jrod4601@gmail.com>
2026-01-27 08:44:28 -0500

    Prepared app for migration

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class TrackedItem(models.Model):
     name = models.CharField(max_length=20)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    price = models.DecimalField(max_length=8)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
 
 
 
--- priceping/settings.py
+++ priceping/settings.py
@@ -44,8 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'priceping',
-    'rest_framework'
+    'accounts.apps.AccountsConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
